9c9e9ea877b43a59fbf8e7784cbcddc9d614473f
Lucas de Lima <54589831+AnimisAle@users.noreply.github.com>
2023-05-10 09:29:04 -0300

    Começando as classes Rei e Torre

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -7,7 +8,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8); 
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            tab.colocarPeca(new Torre(tab,Cor.Amarela), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
