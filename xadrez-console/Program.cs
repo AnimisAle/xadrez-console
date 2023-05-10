eda29415eaa09dbc1a09f6d9c1566120bdc937c1
Lucas de Lima <54589831+AnimisAle@users.noreply.github.com>
2023-05-10 17:42:33 -0300

    mudanças na classe de Tela

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,23 +9,26 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
-            Console.ReadLine();
-            /* try { 
+         //   PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+          //  Console.WriteLine(pos);
+           // Console.WriteLine(pos.toPosicao());
+           // Console.ReadLine();
+             try { 
              Tabuleiro tab = new Tabuleiro(8,8);
 
-             tab.colocarPeca(new Torre(tab,Cor.Amarela), new Posicao(0, 0));
-             tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 9));
-             tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(0, 2));
+             tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+             tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+           
+             tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(4, 2));
+             tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 5));
 
-             Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
              }
              catch (Exception e) {
                  Console.WriteLine(e.Message);    
              }
-             Console.ReadLine(); */
+            // Console.ReadLine(); 
         }
     }
 }
