7726d1a9a104eae4cad3254b2b7217f9b13331e7
Lucas de Lima <54589831+AnimisAle@users.noreply.github.com>
2023-05-15 11:18:16 -0300

    melhorias na classe de rei e torre

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -6,20 +6,14 @@ namespace xadrez_console
 {
     class Tela
     {
-        public static void imprimirTabuleiro(Tabuleiro tab)
-        {
-            for(int i=0;i<tab.linhas;i++)
+        public static void imprimirTabuleiro(Tabuleiro tab){
+
+            for (int i = 0; i < tab.linhas; i++)
             {
                 Console.Write(8 - i + " ");
-                for (int j = 0; j<tab.colunas;j++) {
-                    if (tab.peca(i,j) == null)
-                    {
-                        Console.Write("- ");
-                    }
-                    else {
-                        imprimirPeca(tab.peca(i, j));
-                        Console.Write(' ');
-                    }
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    imprimirPeca(tab.peca(i, j));
                 }
                 Console.WriteLine();
             }
@@ -35,16 +29,24 @@ namespace xadrez_console
 
         public static void imprimirPeca(Peca peca)
         {
-            if(peca.cor == Cor.Branca)
+            if (peca == null)
             {
-                Console.Write(peca);
+                Console.Write("- ");
             }
             else
-            {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor= ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+        { 
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
     }
